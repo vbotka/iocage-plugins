ff235d42be675fa1f50733e0c10e16ed1f0f7c56
Vladimir Botka <vbotka@gmail.com>
2026-06-23 09:17:38 +0200

    export FETCH_TIMEOUT=120

--- post_install.sh
+++ post_install.sh
@@ -31,6 +31,7 @@ EOF2
 	# Apply the hardware checksum and MTU bypass.
 	fix_vnet_interface
 	# Install packages.
+	export FETCH_TIMEOUT=120
 	pkg install -y git py311-ansible sudo syslog-ng
         # Ansible needs UTF-8
         echo "LANG=en_US.UTF-8" >> /root/.profile
@@ -59,6 +60,7 @@ EOF2
 	# Apply the hardware checksum and MTU bypass.
 	fix_vnet_interface
 	# Install packages.
+	export FETCH_TIMEOUT=120
 	pkg install -y git lnav py311-ansible sudo syslog-ng
         # Ansible needs UTF-8
         echo "LANG=en_US.UTF-8" >> /root/.profile
